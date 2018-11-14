23dacc525fcf4af9f266bcf2155b0ef2b728564e
yao <yao.peng@restoplus-tech.cn>
2018-11-14 11:42:18 +0800

    ..

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+// import App from './App';
+//import App from './app/containers/App';
+import App from './src/App';
+
+AppRegistry.registerComponent('MenuApp', () => App);
